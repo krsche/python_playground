ac8d1d9430aba204121c5f6a234c3b88fdad506b
Fabian Kirschner <fabian.kirschner@gmail.com>
2020-12-23 19:59:49 +0100

    upated to enum

--- fourth.py
+++ fourth.py
@@ -1,22 +1,28 @@
+from enum import Enum
+
 class Airport(object):
     def __init__(self, name, country):
         self.name = name
         self.country = country
 
     def __str__(self):
-        return self.name + " (" + self.country + ")"
+        return self.name + " (" + self.country.name + ")"
 
+
+Countries = Enum('Countries', 'Deutschland Australien')
 
 airport_list = [
-    Airport("Berlin Tegel", "Deutschland"),
-    Airport("Stuttgart", "Deutschland"),
-    Airport("Melbourne", "Australien")
+    Airport("Berlin Tegel", Countries.Deutschland),
+    Airport("Stuttgart", Countries.Deutschland),
+    Airport("Melbourne", Countries.Australien)
 ]
 
+# Gut lesbar
 for airport in airport_list:
     print(airport)
 
 print("")
 
+# Schlecht lesbar
 for i in range(len(airport_list)):
     print(airport_list[i])
